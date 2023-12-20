7f2456f4afa2624c39a0a7101af2bba6d1033da6
Grenka054 <alexgrihalex@gmail.com>
2023-12-20 19:47:53 +0700

    imu fix

--- Module 6/ex03/chair/robot_bringup/launch/robot_imu.launch.py
+++ Module 6/ex03/chair/robot_bringup/launch/robot_imu.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2022 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -23,7 +9,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
-from launch.actions import TimerAction
 
 
 def generate_launch_description():
@@ -95,7 +80,5 @@ def generate_launch_description():
         bridge,
         robot_state_publisher,
         rviz,
-        TimerAction(
-            period=5.0,
-            actions=[create])
+        create,
     ])
